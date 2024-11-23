a7d362e6b036b762d19dc6ba064808a2d26f1ab3
roberthsu2003 <roberthsu2003@gmail.com>
2024-11-23 03:54:07 +0000

    lesson4_5

--- lesson4/lesson4_2.py
+++ lesson4/lesson4_2.py
@@ -1,10 +1,10 @@
-import tools
+import widget
 def main():
     while(True):
         try:
             height_cm = float(input("請輸入身高(公分):"))
             weight_kg = float(input("請輸入體重(公斤):"))
-            bmi_value, bmi_str = tools.BMI_math(height_cm, weight_kg)
+            bmi_value, bmi_str = widget.BMI_math(height_cm, weight_kg)
             break
         except Exception:
             print('輸入格式錯誤,請重新輸入!')
@@ -14,4 +14,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
     
