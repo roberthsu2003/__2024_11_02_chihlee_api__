077828264b5a5ccb347f73298b2f3cbd3cae5f93
roberthsu2003 <roberthsu2003@gmail.com>
2024-12-07 06:36:32 +0000

    lesson6_3_2

--- lesson6/lesson6_3.py
+++ lesson6/lesson6_3.py
@@ -2,13 +2,9 @@ from pprint import pprint
 import tools
 
 def main():
-    data:list[dict] = tools.get_aqi(excel_name='aqi.xlsx')
-    sitenames:list = []
-    for item in data:
-        sitenames.append(item['sitename'])    
-
-    sitenames = list(set(sitenames))
-    print(len(sitenames))
+    sitenames:list[str] = tools.get_sitenames(excel_name='aqi.xlsx')
+    print(sitenames)
+  
 
 if __name__ == '__main__':
     main()
