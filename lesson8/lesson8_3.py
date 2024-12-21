ccb2a757236da0bd6c8f48ebed6f0ef390b087c8
roberthsu2003 <roberthsu2003@gmail.com>
2024-12-21 06:24:17 +0000

    lesson8_9

--- lesson8/lesson8_3.py
+++ lesson8/lesson8_3.py
@@ -10,7 +10,13 @@ youbike_data:list[dict] = fetch_youbike_data()
 # 最下方是顯示該行政區域的YouBike站點資訊的地
 area_list = list(set(map(lambda value:value['sarea'],youbike_data)))
 col1,col2 = st.columns([1,3])
-col1.selectbox("行政區域",area_list)
+with col1:
+   selected_area = st.selectbox("行政區域",area_list)
+   
+
+with col2:
+    st.write(selected_area)
+
 # with col1:
 #     selected_sarea = st.selectbox("行政區域",area_list)
 
